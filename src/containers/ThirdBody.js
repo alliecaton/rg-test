c6095d8fe675fea8e5b4bacfd8b6f703523ad656
Allie <alliecaton3@gmail.com>
2021-07-16 11:14:41 -0400

    icon grid

--- src/containers/ThirdBody.js
+++ src/containers/ThirdBody.js
@@ -8,16 +8,18 @@ import calendar from '../assets/home-retention.svg'
 
 const ThirdBody = () => {
 	return (
-		<div>
+		<div className='two-column-grid'>
 			<TextComponent
 				text={
 					'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.'
 				}
 			/>
-			<Icon image={clipboard} title={'LOREM'} />
-			<Icon image={mask} title={'LOREM'} />
-			<Icon image={goggles} title={'LOREM'} />
-			<Icon image={calendar} title={'LOREM'} />
+			<div className='icon-grids'>
+				<Icon image={clipboard} title={'LOREM'} />
+				<Icon image={mask} title={'LOREM'} />
+				<Icon image={goggles} title={'LOREM'} />
+				<Icon image={calendar} title={'LOREM'} />
+			</div>
 		</div>
 	)
 }
