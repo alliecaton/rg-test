74921648a4e7cfe9803d0f137a7054a7de0f4699
Allie <alliecaton3@gmail.com>
2021-07-17 15:19:06 -0400

    media queries for mobile

--- src/containers/FourthBody.js
+++ src/containers/FourthBody.js
@@ -5,19 +5,21 @@ import TextComponent from '../components/shared-elements/TextComponent'
 
 const FourthBody = () => {
 	return (
-		<div className='fourth-body'>
-			<div className='button-wrapper'></div>
-			<img className='image-behind' src={image} alt='child lauging' />
-			<div className='button-wrapper'>
-				<TextComponent
-					title={'LOREM IPSUM'}
-					text={
-						'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.'
-					}
-				/>
-				<OrangeButton />
+		<>
+			<div className='fourth-body'>
+				<img className='image-behind' src={image} alt='child lauging' />
+				<div className='button-wrapper'></div>
+				<div className='button-wrapper'>
+					<TextComponent
+						title={'LOREM IPSUM'}
+						text={
+							'Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.'
+						}
+					/>
+					<OrangeButton />
+				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 
