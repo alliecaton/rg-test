8c4cf88f5ffd8d26264c5566017ad54361466e43
Allie <alliecaton3@gmail.com>
2021-07-17 13:17:30 -0400

    component image flush to top and bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,15 @@ const Footer = () => {
 		<div className='footer-wrapper'>
 			<p className='footer-text'>Follow Us:</p>
 			<div className='social-icons'>
-				<img className='social-icon' src={instagram} alt='instagram logo' />
-				<img className='social-icon' src={facebook} alt='facebook logo' />
+				<a href='https://instagram.com' target='_blank' rel='noreferrer'>
+					<img className='social-icon' src={instagram} alt='instagram logo' />
+				</a>
+				<a
+					href='https://www.facebook.com/RoosterGrin/'
+					target='_blank'
+					rel='noreferrer'>
+					<img className='social-icon' src={facebook} alt='facebook logo' />
+				</a>
 			</div>
 		</div>
 	)
--- src/components/ImageComponent.js
+++ src/components/ImageComponent.js
@@ -3,9 +3,9 @@ import image from './../assets/img-1.jpg'
 
 const ImageComponent = () => {
 	return (
-		<div>
+		<>
 			<img className='component-image' src={image} alt='scenic' />
-		</div>
+		</>
 	)
 }
 
