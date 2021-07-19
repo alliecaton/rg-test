97764dd9cfda7ca959da3045a47b55a895d85298
Allie <alliecaton3@gmail.com>
2021-07-19 10:40:00 -0400

    proper font for welcome in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import PurpleButton from './shared-elements/PurpleButton'
+import image from './../assets/welcome.svg'
 
 const Header = () => {
 	return (
 		<div className='hero-content-parent fade-in-up'>
-			<h1>Welcome to</h1>
+			{/* <h1>Welcome to</h1>
+			 */}
+			<img className='welcome-svg' src={image} alt='welcome to' />
 			<h1>Rooster Grin</h1>
 			<PurpleButton />
 		</div>
